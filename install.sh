f5610e22cb4af843454006811210cd16f820dd9b
Wsgamer7 <ws18022@outlook.com>
2023-03-20 22:28:32 +0800

    add

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ ln -sf ~/dotfiles/runcom/.zshrc ~
 ln -sf ~/dotfiles/runcom/.bash_profile ~
 ln -sf ~/dotfiles/runcom/.inputrc ~
 ln -sf ~/dotfiles/git/.gitconfig ~
+ln -sf ~/dotfiles/config/.tmux.conf ~
+
