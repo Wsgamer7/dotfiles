d5caf83f5b647b5201476e8f2c7611301db12c0d
Wsgamer7 <ws18022@outlook.com>
2023-03-20 21:40:28 +0800

    add_zsh_config

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ln -sf ~/dotfiles/runcom/.bashrc ~
+ln -sf ~/dotfiles/runcom/.zshrc ~
 ln -sf ~/dotfiles/runcom/.bash_profile ~
 ln -sf ~/dotfiles/runcom/.inputrc ~
 ln -sf ~/dotfiles/git/.gitconfig ~
