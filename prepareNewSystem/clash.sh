e7ea59267be8efcff345d79b3481e2e87a5c9892
Wsgamer <ws18022@outlook.com>
2023-04-20 08:44:34 +0800

    modify clash installation

--- prepareNewSystem/clash.sh
+++ prepareNewSystem/clash.sh
@@ -1,4 +1,9 @@
-open https://github.com/Fndroid/clash_for_windows_pkg/releases
+#open https://github.com/Fndroid/clash_for_windows_pkg/releases
+mkdir ~/Documents/app
+cd ~/Documents/app/
+wget https://github.com/Fndroid/clash_for_windows_pkg/releases/download/0.20.21/Clash.for.Windows-0.20.21-x64-linux.tar.gz
+tar xvf Clash*.tar.gz
+echo "open $(pwd)/Clash..."
 echo "open clash and import airports"
 echo "airport: https://dash.fscloud.cc/api/v1/client/subscribe?token=224fc31409df2534a640bcd26cabe121"
 echo "https://dash.fscloud.cc/api/v1/client/subscribe?token=63f0b085e2be95018277e649f55ffe0f"
