1acdc18d5e70d39a41f1124280f63ac6bd8da63e
Shuo Wang <ws18022@outlook.com>
2024-01-06 12:31:49 +0800

    -y for dpkg and flatpak

--- script/gui_app.sh
+++ script/gui_app.sh
@@ -26,16 +26,16 @@ lazygit --version
 
 #google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+yes | sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 #rstudio
 sudo apt install -y rstudio
 
 #QQ
-flatpak install flathub com.qq.QQ
+flatpak install flathub com.qq.QQ -y
 
 #vscode
-sudo apt-get install wget gpg
+sudo apt-get -y install wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -45,4 +45,7 @@ sudo apt update
 sudo apt install -y code # or code-insiders
 
 #figma
-flatpak install flathub io.github.Figma_Linux.figma_linux
+flatpak install flathub io.github.Figma_Linux.figma_linux -y
+
+#yesplaymusic
+flatpak install flathub io.github.qier222.YesPlayMusic -y
