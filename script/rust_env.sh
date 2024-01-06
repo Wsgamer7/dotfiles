34322e682b2faa9cbe932957fc2f7f1fcd467dc2
Shuo Wang <ws18022@outlook.com>
2024-01-06 12:22:11 +0800

    rust sliently install with -y

--- script/rust_env.sh
+++ script/rust_env.sh
@@ -1,3 +1,4 @@
 
 echo "=======installing rust_env========="
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > sh.rustup.rs
+sh ./sh.rustup.rs -y
