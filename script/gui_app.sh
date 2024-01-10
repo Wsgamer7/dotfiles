d18db02863ce25fdbe751d198b4985c584044e9e
Shuo Wang <ws18022@outlook.com>
2024-01-10 17:44:09 +0800

    add zsh to terminal installer

--- script/gui_app.sh
+++ script/gui_app.sh
@@ -6,23 +6,6 @@ echo "=======installing gui app========="
 mkdir -p ~/Documents/app
 cd ~/Documents/app/
 
-sudo apt install -y zsh
-
-#tldr
-pip3 install tldr
-tldr -u
-ln -s $(which fdfind) ~/.local/bin/fd
-
-#install oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-chsh -s /bin/zsh
-
-#lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-lazygit --version
 
 #google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
--- script/terminalApps.sh
+++ script/terminalApps.sh
@@ -4,6 +4,14 @@ echo "=======installing teminal apps========="
 sudo add-apt-repository ppa:aacebedo/fasd
 sudo apt-get update
 
-aptList="fasd fzf tmux python3-pip fd-find neovim curl ripgrep wget "
+aptList="fasd fzf tmux python3-pip fd-find neovim curl ripgrep wget zsh"
 sudo apt-get install -y $aptList
 
+
+#tldr
+pip3 install tldr
+ln -s $(which fdfind) ~/.local/bin/fd
+
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)"
+chsh -s /bin/zsh
